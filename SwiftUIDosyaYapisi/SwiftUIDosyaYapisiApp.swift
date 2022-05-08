94f04a6d19fcd0e1d994527f91590442a8484598
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-08 10:35:06 +0300

    Initial Commit

---
+++ SwiftUIDosyaYapisi/SwiftUIDosyaYapisiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDosyaYapisiApp.swift
+//  SwiftUIDosyaYapisi
+//
+//  Created by Ömer Faruk Kılıçaslan on 8.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDosyaYapisiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
